cefe205cc18cb30cd6e32d7bdd28c54aacf8e804
KostyalBalint <kostyal.balintnak@gmail.com>
2023-10-16 16:02:40 +0200

    cors

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
+import downloader
 
 import objaverse
 from dotenv import load_dotenv
 from flask import Flask, jsonify
 import trimesh
+from flask_cors import CORS, cross_origin
 
-import downloader
 
 load_dotenv(".env")
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 uids = objaverse.load_uids()
 object_paths = downloader.load_object_paths()
@@ -22,6 +25,7 @@ def get_path_by_uid(uid):
 
 # Define a route to get a single item by ID
 @app.route('/annotation/<string:item_id>', methods=['GET'])
+@cross_origin()
 def get_item(item_id):
     try:
         annotation = objaverse.load_annotations([item_id])
@@ -33,6 +37,7 @@ def get_item(item_id):
 
 
 @app.route('/pointCloud/<string:uid>', methods=['GET'])
+@cross_origin()
 def get_point_cloud(uid):
     pc = trimesh.load(get_path_by_uid(uid)).vertices.tolist()
     return jsonify(pc)
